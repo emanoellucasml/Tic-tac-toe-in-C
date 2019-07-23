7ac40b55883d975aad2c12131a75c1f552c56f1e
emanoellucasml <emanoellucas123456@gmail.com>
2019-07-23 10:21:39 -0300

    Adição da função de preenchimento da matriz

--- main.c
+++ main.c
@@ -7,12 +7,21 @@ char intToChar(int algarismo);
 
 int main()
 {
-    //char matriz[3][3] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-    //exibe(matriz);
-    printf("%c\n", intToChar(9));
-
+    char matriz[3][3];
+    preenche(matriz);
+    exibe(matriz);
 
     return 0;
+}
+
+void preenche(char matriz[3][3])
+{
+    int i, j, k = 1;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            matriz[i][j] = intToChar(k++);
+        }
+    }
 }
 
 void exibe(char matriz[3][3])
